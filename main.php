42d70a5a0898bf7a07ec2cbdd7037b3b534d8bba
Goff3110 <goff3110@gmail.com>
2018-10-10 16:32:03 -0500

    Converted site to php

---
+++ main.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Dave's Fortnite Fucker</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <link rel="stylesheet"  type="text/css" media="screen" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.css" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Notable">
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/js/bootstrap.js"></script>
+</head>
+
+<body>
+    <!-- Start Intro Section -->
+    <?php
+        include "intro.html"
+    ?>
+    
+    <!-- Start About Section -->
+    <div class="container-fluid" id="about">
+        <!-- Section Title Row -->
+        <div class="row">
+            <div class="col-sm-12" id="aboutTitle">
+                <h2>All About Me</h2>
+            </div>
+        </div>
+        <!-- Content Container -->
+        <div class="jumbotron">
+            <!-- Bio Row -->
+            <div class="row" id="bioRow">
+                <div class="col-xs-12">
+                    <p>I'm Dave, I love FortNite and assplay and I want to help you get better at both!
+                        Reaching me is simple, just use the form below to schedule a time to meet up online 
+                        and I'll teach you how to maximise your victories and assholes!
+                    </p>
+                </div>
+            </div>
+            <!-- Graphics Row -->
+            <div class="row" id="picRow">
+                <!-- Graphic -->
+                <div class="col-xl">
+                    <img class="img-fluid pic" src="\Images\portal-motel-fortnite.png">
+                </div>
+                <!-- Graphic -->
+                <div class="col-xl">
+                    <img class="img-fluid pic" src="\Images\jigsaw-fortnite.jpg">
+                </div>
+            </div>
+        </div>
+    </div>
+    
+    <!-- Start Contact Section -->
+    <div class="container-fluid" id="contact">
+        
+    </div>
+</body>
+</html>
