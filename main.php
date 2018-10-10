03ab35bdddc37ba651140f69f8c50f64433a7de0
Goff3110 <goff3110@gmail.com>
2018-10-10 17:15:20 -0500

    Cont. conversion to php

--- main.php
+++ main.php
@@ -20,37 +20,9 @@
     ?>
     
     <!-- Start About Section -->
-    <div class="container-fluid" id="about">
-        <!-- Section Title Row -->
-        <div class="row">
-            <div class="col-sm-12" id="aboutTitle">
-                <h2>All About Me</h2>
-            </div>
-        </div>
-        <!-- Content Container -->
-        <div class="jumbotron">
-            <!-- Bio Row -->
-            <div class="row" id="bioRow">
-                <div class="col-xs-12">
-                    <p>I'm Dave, I love FortNite and assplay and I want to help you get better at both!
-                        Reaching me is simple, just use the form below to schedule a time to meet up online 
-                        and I'll teach you how to maximise your victories and assholes!
-                    </p>
-                </div>
-            </div>
-            <!-- Graphics Row -->
-            <div class="row" id="picRow">
-                <!-- Graphic -->
-                <div class="col-xl">
-                    <img class="img-fluid pic" src="\Images\portal-motel-fortnite.png">
-                </div>
-                <!-- Graphic -->
-                <div class="col-xl">
-                    <img class="img-fluid pic" src="\Images\jigsaw-fortnite.jpg">
-                </div>
-            </div>
-        </div>
-    </div>
+    <?php
+        include "about.html"
+    ?>
     
     <!-- Start Contact Section -->
     <div class="container-fluid" id="contact">
